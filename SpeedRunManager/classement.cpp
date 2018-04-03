bd6a54743b6477f81177d566fc156ee4dd4f053f
yoann gouet <yoann.gouet@pc269-14.pedago.ensiie.fr>
2018-04-03 17:28:40 +0200

    ss

---
+++ SpeedRunManager/classement.cpp
@@ -0,0 +1,6 @@
+#include "classement.h"
+
+clasement::clasement()
+{
+
+}
---
+++ SpeedRunManager/classement.h
@@ -0,0 +1,12 @@
+#ifndef CLASEMENT_H
+#define CLASEMENT_H
+
+#include "run.h"
+
+class clasement
+{
+public:
+    clasement();
+};
+
+#endif // CLASEMENT_H
